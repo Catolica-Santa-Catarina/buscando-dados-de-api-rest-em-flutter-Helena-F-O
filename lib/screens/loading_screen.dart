e34307b794920d25141ce2c1f5bf7d55b41cac54
Helena Felicia <helenafeliciaoliveira@gmail.com>
2022-10-19 21:01:29 -0300

    Parte 1 -> Aplicativo Clima -> Helena

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,24 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import '../screens/location_screen.dart';
+import '../services/weather.dart';
+import '../utilities/constants.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
 
   @override
   State<LoadingScreen> createState() => _LoadingScreenState();
+  void initState() {
+    // é disparado quando o Widget for criado
+  }
+
+  Widget? build(BuildContext context) {
+    // disparado quando o objeto for construído e o widget aparecerá na tela
+    return null;
+  }
+
+  void deactivate() {
+    // é disparado quando o widget foi destruído
+  }
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  Future<void> getLocation() async {
+    Location = getCurrentLocation(Latitude, Longitude)
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // obtém a localização atual
-          },
-          child: const Text('Obter Localização'),
-        ),
-      ),
+    return const Scaffold(
+
     );
   }
 }
