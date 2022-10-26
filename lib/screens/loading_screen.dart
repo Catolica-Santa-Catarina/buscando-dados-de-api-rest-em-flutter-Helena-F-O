0bc4cfa737f9e8ed4f4cf7c9468d7aeab5e31eb5
Helena <helenafeliciaoliveira@gmail.com>
2022-10-26 20:33:50 -0300

    Parte 2 -> Aplicativo Clima -> Helena

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,10 +1,16 @@
+import 'dart:convert';
+import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import '../screens/location_screen.dart';
+import '../services/networking.dart';
 import '../services/weather.dart';
 import '../utilities/constants.dart';
 import 'package:flutter/src/widgets/navigator.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+const apiKey = '48dffcee1ec94cedee47ee637046b9bc';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -15,9 +21,14 @@ class LoadingScreen extends StatefulWidget {
     // é disparado quando o Widget for criado
   }
 
-  Widget? build(BuildContext context) {
-    // disparado quando o objeto for construído e o widget aparecerá na tela
-    return null;
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: SpinKitDoubleBounce(
+        color: Colors.white,
+        size: 100.0,
+      ),
+    );
   }
 
   void deactivate() {
@@ -27,32 +38,36 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
-  @override
-  void initState() {
-    super.initState();
-    getLocation();
-  }
+  late double latitude;
+  late double longitude;
 
-  void getLocation() async {
-    var weatherData = await WeatherModel().getLocationWeather();
-
+  void pushToLocationScreen(dynamic weatherData) {
     Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return LocationScreen(
-        getLocationWeather: weatherData,
-      );
+      return LocationScreen(locationWeather: weatherData, getLocationWeather: null,);
     }));
   }
 
-  void getData() async {
-    var url = Uri.parse('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
-    http.Response response = await http.get(url);
+  @override
+  void initState() {
+    super.initState();
+    updateUI(widget.locationWeather);
+  }
 
-    if (response.statusCode == 200) { // se a requisição foi feita com sucesso
-      var data = response.body;
-      print(data);  // imprima o resultado
-    } else {
-      print(response.statusCode);  // senão, imprima o código de erro
-    }
+  Future<void> getLocation() async {
+    var location = Location();
+    await location.getCurrentPosition();
+
+    latitude = location.latitude!;
+    longitude = location.longitude!;
+
+    getData();
+  }
+
+  void getData() async {
+    NetworkHelper networkHelper = NetworkHelper('https://api.openweathermap.org/'
+        'data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
   }
 
   @override
